d31f5d3e633a2da31867be956bc7f5ae891f3b12
ntdvl <yilmazkoman@outlook.com>
2018-04-22 15:55:26 +0300

    Ek Çalışma

---
+++ 1. Gün/8. Calisma/app.js
@@ -0,0 +1,11 @@
+let movie = 'Matrix';
+
+function test() {
+
+	let movie = 'The Revenant';
+	return movie;
+
+}
+
+console.log(movie);
+console.log(test());
---
+++ 1. Gün/9. Calisma/app.js
@@ -0,0 +1,7 @@
+function area(r) {
+
+    var PI = 3.14;
+    return PI * r * r;
+}
+
+console.log(area(5));
